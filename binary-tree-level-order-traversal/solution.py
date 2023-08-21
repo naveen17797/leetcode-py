dc1097f57aff18a79ff42f292633c7d5f2f54e23
naveen <kmnaveen101@gmail.com>
2023-08-21 11:55:34 +0530

    todo complete binary tree level order traversal

---
+++ binary-tree-level-order-traversal/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def example_method():
+        pass
+
+
+if __name__ == '__main__':
+    s = Solution()
---
+++ same-tree/solution.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        stack = [p, q]
+        while stack:
+            n1 = stack.pop()
+            n2 = stack.pop()
+            if n1 is None or n2 is None:
+                return False
+            if n1.val != n2.val:
+                return False
+            stack.append(n1.left)
+            stack.append(n2.left)
+            stack.append(n1.right)
+            stack.append(n2.right)
+        return True
