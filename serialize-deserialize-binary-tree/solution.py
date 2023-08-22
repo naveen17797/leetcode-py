20c98222048d620520644b3a0ef0c5f726e0609a
naveen <kmnaveen101@gmail.com>
2023-08-22 08:31:10 +0530

    serialize deserialize binary tree

---
+++ serialize-deserialize-binary-tree/solution.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        result = []
+        def dfs(node):
+            if not node:
+                result.append("N")
+                return None
+            result.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(result)
+
+    def deserialize(self, data):
+        values = data.split(",")
+        self.i = 0
+        def dfs():
+            if values[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(values[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        return dfs()
