7d2c8a5c937321ed292f43bd2ad2d10f4d5d1295
naveen <kmnaveen101@gmail.com>
2023-08-23 14:25:48 +0530

    add, search words

--- design-add-and-search-add-words/solution.py
+++ design-add-and-search-add-words/solution.py
@@ -1,35 +1,42 @@
 class TrieNode:
-    def __init__(self, word_end):
-        self.word_end = word_end
+    def __init__(self):
+        self.word_end = False
         self.nodes = {}
 
 
 class WordDictionary:
 
     def __init__(self):
-        self.root = TrieNode(False)
+        self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
         head = self.root
         for c in word:
-            if head.nodes[c] is None:
-                head.nodes[c] = TrieNode(False)
-            else:
-                head = head.nodes[c]
-        next.word_end = False
+            if c not in head.nodes:
+                head.nodes[c] = TrieNode()
+            head = head.nodes[c]
+        head.word_end = True
 
     def search(self, word: str) -> bool:
-        head = self.root
-        for c in word:
-            if c == ".":
-                pass
-            else:
-                if head.nodes[c] is None:
+
+        def dfs(j, root):
+            head = root
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for node in head.nodes.values():
+                        if dfs(i + 1, node):
+                            return True
                     return False
                 else:
+                    if c not in head.nodes:
+                        return False
                     head = head.nodes[c]
-        return head.word_end
+            return head.word_end
 
+        return dfs(0, self.root)
 
-if __name__ == '__main__':
-    s = WordDictionary()
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
