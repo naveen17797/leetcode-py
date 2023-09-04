5fe96cb66f1a0a0385e04a3ab9847b77d003d0e6
naveen <kmnaveen101@gmail.com>
2023-09-04 06:32:45 +0530

    subarray sum equals k

---
+++ subarray-sum-equals-k/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        cumulative_sum, count = 0,0
+        prefix_map = {0:1}
+        for num in nums:
+            cumulative_sum += num
+            if cumulative_sum - k in prefix_map:
+                count += prefix_map.get(cumulative_sum, 0)
+            prefix_map[cumulative_sum] = prefix_map.get(cumulative_sum, 0) + 1
+        return count
+
+
+if __name__ == '__main__':
+    s = Solution()
