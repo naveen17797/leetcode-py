0c878159521c86879ab89876a26737fa27d651ea
naveen <kmnaveen101@gmail.com>
2023-09-05 17:37:08 +0530

    distinct subsequences dfs

---
+++ distinct-subsequences/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        cache = {}
+        s_len, t_len = len(s), len(t)
+        def dfs(i, j):
+            if j == t_len: return 1
+            if i == s_len: return 0
+            if (i,j) in cache: return cache[(i,j)]
+
+            # if characters match, then we have two options
+            # match the character or ignore the character and try to match the next
+            if s[i] == t[j]:
+                cache[(i,j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
+            else:
+                # if they dont match the only way is to look forward
+                cache[(i,j)] = dfs(i+1, j)
+
+            return cache[(i,j)]
+        return dfs(0,0)
+
+
+if __name__ == '__main__':
+    s = Solution()
