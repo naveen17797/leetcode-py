f2ffe6e1598f2c297f5b9e1e059f3976e4811ddc
naveen <kmnaveen101@gmail.com>
2023-09-19 17:48:26 +0530

    formatting

--- network-delay-time-using-dijsktra/solution.py
+++ network-delay-time-using-dijsktra/solution.py
@@ -1,5 +1,34 @@
+import heapq
+from collections import defaultdict
+from typing import List
+
 class Solution:
-    def example_method():
-        pass
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        for u, v, time_took_to_reach_parent_vertex in times:
+            graph[u].append([time_took_to_reach_parent_vertex, v])
+
+        result = [float("+inf")] * n
+        result[k - 1] = 0
+        visited = set()
+        visited.add(k)
+
+        min_heap = [[0,k]]
+
+        while min_heap:
+            time_took_to_reach_parent_vertex, parent_vertex = heapq.heappop(min_heap)
+
+            result[parent_vertex - 1] = min(result[parent_vertex - 1], time_took_to_reach_parent_vertex)
+
+            for connected_vertex_time, connected_vertex in graph[parent_vertex]:
+                total_time = time_took_to_reach_parent_vertex + connected_vertex_time
+                if total_time < result[connected_vertex-1]:
+                    result[connected_vertex-1] = total_time
+                    heapq.heappush(min_heap, [connected_vertex_time + time_took_to_reach_parent_vertex, connected_vertex])
+
+        minimum_time_taken = max(result)
+        return minimum_time_taken if minimum_time_taken != float("inf") else -1
+
 if __name__ == '__main__':
     s = Solution()
+    s.networkDelayTime([[2,1,1],[2,3,1],[3,4,1]], 4, 2)
--- swim-in-rising-water-dijsktra/solution.py
+++ swim-in-rising-water-dijsktra/solution.py
@@ -1,30 +1,27 @@
+import heapq
 from heapq import heappop, heappush
 from typing import List
 
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        output = 0
-        visited = set()
-        min_heap = [(grid[0][0], 0, 0)]
-        directions = [ [-1,0], [1,0], [0,1], [0,-1] ]
-
         ROWS, COLS = len(grid), len(grid[0])
+        min_heap = [[grid[0][0], 0, 0]]
+        max_val = grid[0][0]
+        directions = [[0,1], [1,0], [0,-1], [-1,0]]
+        visited = set()
 
         while min_heap:
-            height, r, c = heappop(min_heap)
-            output = max(output, height)
-            if r == ROWS -1 and c == COLS - 1:
+            val, r, c = heapq.heappop(min_heap)
+            max_val = max(val, max_val)
+            if r == ROWS - 1 and c == COLS-1:
                 break
-
             for dx, dy in directions:
-                rx, cy = r + dx, c + dy
-                if 0 <= rx < ROWS and 0 <= cy < COLS and (rx, cy) not in visited:
-                    heappush(min_heap, (grid[rx][cy], rx, cy))
-                    visited.add((r,c))
-        return output
-
-
+                tx, ty = r+dx, c+dy
+                if 0 <= tx < ROWS and 0 <= ty < COLS and (tx,ty) not in visited:
+                    heapq.heappush(min_heap, [grid[tx][ty], tx, ty])
+                    visited.add((tx,ty))
+        return max_val
 
 
 if __name__ == '__main__':
